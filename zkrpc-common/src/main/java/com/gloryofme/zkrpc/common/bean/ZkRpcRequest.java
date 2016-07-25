25aa2717fc795eb1697ab7e0ea14ce114c5a835b
gloryofme <gloryofme@163.com>
2016-07-25 17:36:45 +0800

    rpc请求

---
+++ zkrpc-common/src/main/java/com/gloryofme/zkrpc/common/bean/ZkRpcRequest.java
@@ -0,0 +1,54 @@
+package com.gloryofme.zkrpc.common.bean;
+
+/**
+ * rpc请求
+ * @author gloryzyf<bupt_zhuyufei@163.com>
+ */
+public class ZkRpcRequest {
+
+    private String interfaceName;
+    private String methodName;
+    private String serviceVersion;
+    private Class<?>[] paramTypes;
+    private Object[] params;
+
+    public String getInterfaceName() {
+        return interfaceName;
+    }
+
+    public void setInterfaceName(String interfaceName) {
+        this.interfaceName = interfaceName;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+
+    public String getServiceVersion() {
+        return serviceVersion;
+    }
+
+    public void setServiceVersion(String serviceVersion) {
+        this.serviceVersion = serviceVersion;
+    }
+
+    public Class<?>[] getParamTypes() {
+        return paramTypes;
+    }
+
+    public void setParamTypes(Class<?>[] paramTypes) {
+        this.paramTypes = paramTypes;
+    }
+
+    public Object[] getParams() {
+        return params;
+    }
+
+    public void setParams(Object[] params) {
+        this.params = params;
+    }
+}
