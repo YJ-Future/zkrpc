42fb035a9ddfe012c8e7b48300ec3e0c8b6061b3
gloryofme <gloryofme@163.com>
2016-08-25 13:52:45 +0800

    全局常量

---
+++ zkrpc-service-zk/src/main/java/com/gloryofme/zkrpc/service/zk/ZkConstants.java
@@ -0,0 +1,12 @@
+package com.gloryofme.zkrpc.service.zk;
+
+/**
+ * 全局常量
+ * @author gloryzyf<bupt_zhuyufei@163.com>
+ */
+public class ZkConstants {
+    public static int ZK_SESSION_TIMEOUT = 5000;
+    public static int ZK_CONNECTION_TIMEOUT = 1000;
+
+    public static String ZK_REGISTRY_PATH = "/zk_service";
+}
